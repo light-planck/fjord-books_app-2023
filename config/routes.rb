340b6e750f96806caf6a86b138ee99685d8716cf
light-planck <seseifunc16@gmail.com>
2023-12-05 17:03:56 +0900

    feat: users一覧ページと詳細ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'users/index'
   devise_for :users
   resources :books
-  resources :users, only: [:index]
+  resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
