934fa6f72d711567b28e2a3b38696c3b03c1420a
light-planck <seseifunc16@gmail.com>
2023-12-06 16:12:09 +0900

    feat: 日報の新規作成画面の作成

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,26 +1,45 @@
 # frozen_string_literal: true
 
 class ReportsController < ApplicationController
+  before_action :set_report, only: %i[show edit update destroy]
+
   def index
     @reports = Report.order(:id).page(params[:page])
   end
 
-  def show
+  def show; end
+
+  def new
+    @report = Report.new
+  end
+
+  def edit; end
+
+  def create
+    @report = Report.new(report_params)
+
+    respond_to do |format|
+      if @report.save
+        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human) }
+        format.json { render :show, status: :created, location: @report }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @report.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update; end
+
+  def destroy; end
+
+  private
+
+  def set_report
     @report = Report.find(params[:id])
   end
 
-  def new
-  end
-
-  def edit
-  end
-
-  def create
-  end
-
-  def update
-  end
-
-  def destroy
+  def report_params
+    params.require(:report).permit(:title, :content)
   end
 end
