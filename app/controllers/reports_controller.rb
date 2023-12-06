e6dc0e7c7ab1a29285606388ab8038e7ff997202
light-planck <seseifunc16@gmail.com>
2023-12-06 15:50:08 +0900

    feat: reportsコントローラーの作成

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 class ReportsController < ApplicationController
   def index
+    @reports = Report.order(:id).page(params[:page])
   end
 
   def show
+    @report = Report.find(params[:id])
   end
 
   def new
