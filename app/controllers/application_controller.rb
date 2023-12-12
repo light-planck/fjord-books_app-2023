2b3572759aed8186380d11f4b3c04ec964d453ac
light-planck <seseifunc16@gmail.com>
2023-12-12 17:13:34 +0900

    feat: strong parametersを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    keys = %i[name zip_code address personal_statement]
+    devise_parameter_sanitizer.permit(:sign_up, keys: keys)
+    devise_parameter_sanitizer.permit(:account_update, keys: keys)
+  end
 end
