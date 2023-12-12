236eb2600f1e6700a5ef1c3eeff35a18128df1b3
light-planck <seseifunc16@gmail.com>
2023-12-12 17:37:10 +0900

    style: lint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     keys = %i[name zip_code address personal_statement]
-    devise_parameter_sanitizer.permit(:sign_up, keys: keys)
-    devise_parameter_sanitizer.permit(:account_update, keys: keys)
+    devise_parameter_sanitizer.permit(:sign_up, keys:)
+    devise_parameter_sanitizer.permit(:account_update, keys:)
   end
 end
