1a99943d8ee1a560a0507a83cd19bbacb6db41ef
light-planck <seseifunc16@gmail.com>
2023-12-14 18:00:41 +0900

    feat: メンションの関連付けを行なった

--- app/models/report.rb
+++ app/models/report.rb
@@ -7,11 +7,30 @@ class Report < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
+  has_many :mentions_out, class_name: 'ReportMention', foreign_key: :mentioned_id, dependent: :destroy
+  has_many :mentioning_reports, through: :mentions_out, source: :mentioning
+
+  has_many :mentions_in, class_name: 'ReportMention', foreign_key: :mentioning_id, dependent: :destroy
+  has_many :mentioned_reports, through: :mentions_in, source: :mentioned
+
+  after_save :update_mentions
+
   def editable?(target_user)
     user == target_user
   end
 
   def created_on
     created_at.to_date
+  end
+
+  private
+
+  def update_mentions
+    mentions_out.destroy_all
+
+    content.scan(/http:\/\/localhost:3000\/reports\/(\d+)/).uniq.each do |report_id|
+      report = Report.find_by(id: report_id)
+      mentioning_reports << report if report
+    end
   end
 end
