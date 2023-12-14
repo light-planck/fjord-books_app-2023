b6fc65ca500e06584718808bbf07461ca4497a96
light-planck <seseifunc16@gmail.com>
2023-12-14 18:55:20 +0900

    style: lint

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
 class Report < ApplicationRecord
+  REPORT_URL_REGEX = %r{http://localhost:3000/reports/\d+}
+
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
   validates :title, presence: true
   validates :content, presence: true
 
-  has_many :mentions_out, class_name: 'ReportMention', foreign_key: :mentioned_id, dependent: :destroy
+  has_many :mentions_out, class_name: 'ReportMention', foreign_key: :mentioned_id, dependent: :destroy, inverse_of: :mentioned
   has_many :mentioning_reports, through: :mentions_out, source: :mentioning
 
-  has_many :mentions_in, class_name: 'ReportMention', foreign_key: :mentioning_id, dependent: :destroy
+  has_many :mentions_in, class_name: 'ReportMention', foreign_key: :mentioning_id, dependent: :destroy, inverse_of: :mentioning
   has_many :mentioned_reports, through: :mentions_in, source: :mentioned
 
   after_save :update_mentions
@@ -28,7 +30,7 @@ class Report < ApplicationRecord
   def update_mentions
     mentions_out.destroy_all
 
-    content.scan(/http:\/\/localhost:3000\/reports\/(\d+)/).uniq.each do |report_id|
+    content.scan(REPORT_URL_REGEX).uniq.each do |report_id|
       report = Report.find_by(id: report_id)
       mentioning_reports << report if report
     end
