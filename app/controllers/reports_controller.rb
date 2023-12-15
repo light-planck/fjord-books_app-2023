f89de6c7e0aa0e468cc056aee95dc750533d6d31
light-planck <seseifunc16@gmail.com>
2023-12-15 17:04:18 +0900

    style: lint

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -34,7 +34,7 @@ class ReportsController < ApplicationController
     if @report.update(report_params)
       redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
-        render :edit, status: :unprocessable_entity
+      render :edit, status: :unprocessable_entity
     end
   end
 
