98b20583a160b60f74b60dbfc274344ac3ede1ad
light-planck <seseifunc16@gmail.com>
2023-12-19 18:46:38 +0900

    feat: Userモデルのメソッドをテスト

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#name_or_email' do
+    user = users(:one)
+    assert_equal 'test@example.com', user.name_or_email
+
+    user2 = users(:two)
+    assert_equal 'test2', user2.name_or_email
+  end
 end
