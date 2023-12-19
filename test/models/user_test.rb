3b21e699fa37e9beb29858509ca8c03e841299b9
light-planck <seseifunc16@gmail.com>
2023-12-19 18:58:05 +0900

    refactor: フィクスチャを修正

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,10 +4,10 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test '#name_or_email' do
-    user = users(:one)
-    assert_equal 'test@example.com', user.name_or_email
+    user = users(:user1)
+    assert_equal 'user1@example.com', user.name_or_email
 
-    user2 = users(:two)
-    assert_equal 'test2', user2.name_or_email
+    user2 = users(:user2)
+    assert_equal 'User 2', user2.name_or_email
   end
 end
