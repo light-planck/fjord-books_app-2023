6cccf3350a7845d41b964a89aa763efc18bf1a40
light-planck <seseifunc16@gmail.com>
2023-12-19 18:59:32 +0900

    feat: Reportモデルのeditableのテスト

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,12 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    user1 = users(:user1)
+    user2 = users(:user2)
+    report1 = reports(:report1)
+
+    assert report1.editable?(user1)
+    assert_not report1.editable?(user2)
+  end
 end
