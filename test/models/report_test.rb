34a0178a01bb0f128278a51b42b57598ff5e16b8
light-planck <seseifunc16@gmail.com>
2023-12-27 11:38:37 +0900

    refactor: fixtureの変数名を変更

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -6,10 +6,10 @@ class ReportTest < ActiveSupport::TestCase
   test '#editable?' do
     user1 = users(:user1)
     user2 = users(:user2)
-    report1 = reports(:report1)
+    report = reports(:report1)
 
-    assert report1.editable?(user1)
-    assert_not report1.editable?(user2)
+    assert report.editable?(user1)
+    assert_not report.editable?(user2)
   end
 
   test '#created_on' do
