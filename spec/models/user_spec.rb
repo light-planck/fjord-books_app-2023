fdf5d6233c47ab6c9869842f58f4c2054e17749d
light-planck <seseifunc16@gmail.com>
2024-01-31 19:16:15 +0900

    feat: user modelのテストを作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,17 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe '#name_or_email' do
+    it 'returns the name when the user has a name' do
+      has_name_user = FactoryBot.create(:user)
+      expect(has_name_user.name_or_email).to eq(has_name_user.name)
+    end
+
+    it 'returns the email when the user does not have a name' do
+      has_no_name_user = FactoryBot.create(:user, name: nil)
+      expect(has_no_name_user.name_or_email).to eq(has_no_name_user.email)
+    end
+  end
 end
