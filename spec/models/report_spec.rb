bfe19ea0452ba37c106a704dc5c2d90930a7d3e7
light-planck <seseifunc16@gmail.com>
2024-01-31 19:06:30 +0900

    feat: created_onのテストを作成

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -3,17 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe Report, type: :model do
-  describe '#editable?' do
-    let(:user1) { FactoryBot.create(:user) }
-    let(:user2) { FactoryBot.create(:user) }
-    let(:report) { FactoryBot.create(:report, user: user1) }
+  let(:user1) { FactoryBot.create(:user) }
+  let(:user2) { FactoryBot.create(:user) }
+  let(:report) { FactoryBot.create(:report, user: user1) }
 
+  describe '#editable?' do
     it 'returns true when the report is created by the user' do
       expect(report.editable?(user1)).to be_truthy
     end
 
     it 'returns false when the report is not created by the user' do
       expect(report.editable?(user2)).to be_falsey
+    end
+  end
+
+  describe '#created_on' do
+    it 'returns the date when the report is created' do
+      report.created_at = Time.zone.local(2024, 1, 1, 0, 0, 0)
+
+      expect(report.created_on).to eq(Date.new(2024, 1, 1))
     end
   end
 end
