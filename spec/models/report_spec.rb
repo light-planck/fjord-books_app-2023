2a0dd439521686833b398c7063dbb8613a6d8f71
light-planck <seseifunc16@gmail.com>
2024-01-31 19:35:10 +0900

    feat: reportのモデルメソッドのテストを作成

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -24,4 +24,32 @@ RSpec.describe Report, type: :model do
       expect(report.created_on).to eq(Date.new(2024, 1, 1))
     end
   end
+
+  describe '#save_mentions' do
+    let(:report1) { FactoryBot.create(:report) }
+    let(:report2) { FactoryBot.create(:report, content: "http://localhost:3000/reports/#{report1.id}}") }
+    let(:report3) { FactoryBot.create(:report, content: "http://localhost:3000/reports/#{report1.id} http://localhost:3000/reports/#{report2.id}") }
+
+    it 'saves mention' do
+      expect(report2.mentioning_reports).to eq [report1]
+    end
+
+    it 'save multiple mentions' do
+      expect(report3.mentioning_reports).to eq [report1, report2]
+    end
+
+    it 'is updated when the report is updated' do
+      report3.update!(content: 'http://localhost:3000/reports/1')
+      report3.reload
+
+      expect(report3.mentioning_reports).to eq [report1]
+    end
+
+    it 'is updated when the report is deleted' do
+      report2.destroy!
+      report3.destroy!
+
+      expect(report1.mentioning_reports).to eq []
+    end
+  end
 end
