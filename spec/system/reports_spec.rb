b00a9e3ebf0445d54973514620a16a0f0805b5c3
light-planck <seseifunc16@gmail.com>
2024-02-01 18:23:11 +0900

    feat: 日報編集システムテストの作成

--- spec/system/reports_spec.rb
+++ spec/system/reports_spec.rb
@@ -29,18 +29,19 @@ RSpec.describe 'Reports', type: :system do
     report = FactoryBot.create(:report, user:)
     visit reports_url
 
-    expect do
-      click_on 'この日報を表示', match: :first
-      assert_text '日報の詳細'
+    click_on 'この日報を表示', match: :first
+    assert_text '日報の詳細'
 
-      click_on 'この日報を編集'
-      assert_text '日報の編集'
+    click_on 'この日報を編集'
+    assert_text '日報の編集'
 
-      fill_in 'タイトル', with: 'フィヨルドブートキャンプの感想'
-      fill_in '内容', with: '学びが多い'
-      click_on '更新する'
-    end.to change { report.reload.title }.from(report.title).to('フィヨルドブートキャンプの感想')
+    fill_in 'タイトル', with: 'フィヨルドブートキャンプの感想'
+    fill_in '内容', with: '学びが多い'
+    click_on '更新する'
 
     assert_text '日報が更新されました。'
+
+    expect(report.reload.title).to eq 'フィヨルドブートキャンプの感想'
+    expect(report.reload.content).to eq '学びが多い'
   end
 end
